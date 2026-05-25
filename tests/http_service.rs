2b540eca29031dcfe8f95542d183b14767041d93
Jose Fernandez (magec) <joseferper@gmail.com>
2026-05-25 09:19:13 +0200

    Add simple service struct to emulate runing services

---
+++ tests/http_service.rs
@@ -0,0 +1,143 @@
+use std::net::{Ipv4Addr, SocketAddr};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tokio::sync::oneshot;
+use tokio::task::JoinHandle;
+
+pub struct HttpService {
+    port: u16,
+    shutdown: Option<oneshot::Sender<()>>,
+    task: Option<JoinHandle<()>>,
+}
+
+impl HttpService {
+    pub async fn new() -> std::io::Result<Self> {
+        let mut service = Self {
+            port: 0,
+            shutdown: None,
+            task: None,
+        };
+        service.start().await?;
+        Ok(service)
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub fn is_running(&self) -> bool {
+        self.task
+            .as_ref()
+            .map(|task| !task.is_finished())
+            .unwrap_or(false)
+    }
+
+    pub async fn start(&mut self) -> std::io::Result<()> {
+        if self.is_running() {
+            return Ok(());
+        }
+
+        let listener = TcpListener::bind(SocketAddr::from((Ipv4Addr::LOCALHOST, 0))).await?;
+        self.port = listener.local_addr()?.port();
+
+        let (shutdown_tx, shutdown_rx) = oneshot::channel();
+        self.shutdown = Some(shutdown_tx);
+        self.task = Some(tokio::spawn(run_server(listener, shutdown_rx)));
+
+        Ok(())
+    }
+
+    pub async fn stop(&mut self) {
+        if let Some(shutdown) = self.shutdown.take() {
+            let _ = shutdown.send(());
+        }
+
+        if let Some(task) = self.task.take() {
+            let _ = task.await;
+        }
+    }
+}
+
+impl Drop for HttpService {
+    fn drop(&mut self) {
+        if let Some(shutdown) = self.shutdown.take() {
+            let _ = shutdown.send(());
+        }
+
+        if let Some(task) = self.task.take() {
+            task.abort();
+        }
+    }
+}
+
+async fn run_server(listener: TcpListener, shutdown: oneshot::Receiver<()>) {
+    tokio::pin!(shutdown);
+
+    loop {
+        tokio::select! {
+            _ = &mut shutdown => break,
+            accepted = listener.accept() => {
+                let Ok((mut socket, _)) = accepted else {
+                    continue;
+                };
+
+                tokio::spawn(async move {
+                    let mut buffer = [0_u8; 4096];
+                    let _ = socket.read(&mut buffer).await;
+
+                    let response = concat!(
+                        "HTTP/1.1 200 OK\r\n",
+                        "Content-Type: text/plain\r\n",
+                        "Content-Length: 2\r\n",
+                        "Connection: close\r\n",
+                        "\r\n",
+                        "OK",
+                    );
+
+                    let _ = socket.write_all(response.as_bytes()).await;
+                    let _ = socket.shutdown().await;
+                });
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn starts_on_random_port_and_responds() {
+        let service = HttpService::new().await.unwrap();
+
+        assert!(service.port() > 0);
+        assert!(service.is_running());
+
+        let response = reqwest::get(format!("http://127.0.0.1:{}/", service.port()))
+            .await
+            .unwrap();
+
+        assert!(response.status().is_success());
+        assert_eq!(response.text().await.unwrap(), "OK");
+    }
+
+    #[tokio::test]
+    async fn can_stop_and_start_again() {
+        let mut service = HttpService::new().await.unwrap();
+        let first_port = service.port();
+
+        service.stop().await;
+        assert!(!service.is_running());
+
+        service.start().await.unwrap();
+        assert!(service.is_running());
+        assert!(service.port() > 0);
+
+        let response = reqwest::get(format!("http://127.0.0.1:{}/", service.port()))
+            .await
+            .unwrap();
+
+        assert!(response.status().is_success());
+        assert_ne!(first_port, 0);
+    }
+}
